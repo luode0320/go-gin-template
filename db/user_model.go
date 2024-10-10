48dd1602feadf7679550af1415afbeb2892770e6
罗德 <1846555387@qq.com>
2024-10-11 00:15:10 +0800

    feat: 补充主键信息

--- db/user_model.go
+++ db/user_model.go
@@ -1,11 +1,14 @@
 // models/user_model.go
 package db
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
-	Name string `form:"name" json:"name" validate:"required,min=3,max=50"`
+	Name string `form:"name" json:"name" validate:"required,min=3,max=50"  gorm:"unique;type:varchar(255)"`
+	QQ   string `form:"qq" json:"qq" gorm:"type:varchar(255)"`
 }
 
 func (User) TableName() string {
